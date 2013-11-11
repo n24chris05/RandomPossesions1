963f15525ab1a9d4f34298f1f33824999f8491de
n24chris05 <ChrisMacBookPro@Chriss-MacBook-Pro.local>
2013-11-12 01:03:54 +0800

    trial

---
+++ trial/trial/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  trial
+//
+//  Created by Chris Arquelada on 11/12/13.
+//  Copyright (c) 2013 Chris Arquelada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
